33f360a05091ee146bd1384dec1608ae9b55f4f9
Christophe Thibaut <cthibaut@octo.com>
2020-08-12 23:21:00 +0200

    we can pass the test with fixed parameters

--- CPTTRN1/cpttrn1.fs
+++ CPTTRN1/cpttrn1.fs
@@ -3,20 +3,31 @@ CHAR * CONSTANT STAR
 
 \ put a star if n is even, else a dot 
 : DOT-OR-CHAR ( n -- c )
-    1 AND IF DOT ELSE CHAR THEN ;
+    1 AND IF DOT ELSE STAR THEN ;
     
 \ print the character depending on n
 : .DOT-OR-CHAR ( n -- )
     DOT-OR-CHAR EMIT ;
 
-\ print n rows of 1 star or dot
-: .ROWS ( n -- )
-    0 SWAP
+\ print a row of n stars, with initial counter
+: .ROW  ( c,n )
     0 DO
-        DUP .DOT-OR-CHAR CR
-        1+
+        DUP .DOT-OR-CHAR 1+
     LOOP 
-    DROP ;
+    DROP CR ;
+        
+    
+\ print n rows of m star or dot
+: .ROWS ( n,m -- )
+    SWAP          \ m,n        outer loop max on top
+    0 DO          \ m
+        I OVER    \ m,c,m      counter = I, keep m for several loops
+        .ROW
+    LOOP 
+    DROP CR ;
 
-3 .ROWS
+3 1 .ROWS
+4 4 .ROWS
+2 5 .ROWS
+
 BYE

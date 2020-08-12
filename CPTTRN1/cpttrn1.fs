226d841ed4f9863e8eee0f6830f7339e7e96e7a6
Christophe Thibaut <cthibaut@octo.com>
2020-08-12 22:52:20 +0200

    we can use a loop to print several rows

--- CPTTRN1/cpttrn1.fs
+++ CPTTRN1/cpttrn1.fs
@@ -9,8 +9,14 @@ CHAR * CONSTANT STAR
 : .DOT-OR-CHAR ( n -- )
     DOT-OR-CHAR EMIT ;
 
-0 .DOT-OR-CHAR CR
-1 .DOT-OR-CHAR CR
-2 .DOT-OR-CHAR CR
+\ print n rows of 1 star or dot
+: .ROWS ( n -- )
+    0 SWAP
+    0 DO
+        DUP .DOT-OR-CHAR CR
+        1+
+    LOOP 
+    DROP ;
 
+3 .ROWS
 BYE

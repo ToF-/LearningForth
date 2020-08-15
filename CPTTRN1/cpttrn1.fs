0ce31292324d6cb53648fe66cdb51947e05415fb
Christophe Thibaut <cthibaut@octo.com>
2020-08-15 21:53:48 +0200

    refactoring

--- CPTTRN1/cpttrn1.fs
+++ CPTTRN1/cpttrn1.fs
@@ -1,61 +1,38 @@
-CHAR . CONSTANT DOT
-CHAR * CONSTANT STAR
-
-\ put a star if n is even, else a dot 
-: DOT-OR-CHAR ( n -- c )
-    1 AND IF DOT ELSE STAR THEN ;
+: STARDOT  ( n -- char )
+    1 AND IF [CHAR] . ELSE [CHAR] * THEN ;
     
-\ print the character depending on n
-: .DOT-OR-CHAR ( n -- )
-    DOT-OR-CHAR EMIT ;
-
-\ print a row of n stars, with initial counter
 : .ROW  ( c,n )
-    0 DO
-        DUP .DOT-OR-CHAR 1+
-    LOOP 
+    0 DO DUP STARDOT EMIT 1+ LOOP 
     DROP CR ;
         
-    
-\ print n rows of m star or dot
-: .ROWS ( n,m -- )
-    SWAP          \ m,n        outer loop max on top
-    0 DO          \ m
-        I OVER    \ m,c,m      counter = I, keep m for several loops
-        .ROW
-    LOOP 
+: .ROWS ( #cols,#rows -- )
+    0 DO I OVER .ROW LOOP 
     DROP CR ;
 
-\ true if the given char is between '0' and '9'
-: IS-DIGIT? ( c -- f )
-    DUP  [CHAR] 0 >=
-    SWAP [CHAR] 9 <= AND ;
+: TO-DIGIT ( char -- n )
+    [CHAR] 0 - ;
 
-\ read input until first digit char, return it
-: SKIP-NON-DIGIT ( -- c )
+: IS-DIGIT? ( char -- flag )
+    TO-DIGIT DUP 0 >= SWAP 9 <= AND ;     
+
+: SKIP-NON-DIGIT ( -- char )
     BEGIN
         KEY DUP IS-DIGIT? 0= WHILE
         DROP
     REPEAT ;
 
-\ read input until a number is read, return it
 : GET-NUMBER ( -- n )
-    SKIP-NON-DIGIT  \ c
-    0 SWAP          \ a,c     put the accumulator
+    SKIP-NON-DIGIT  
+    0 SWAP          \ accumulator
     BEGIN
-        [CHAR] 0 -  \ a,d     digit value
-        SWAP 10 * + \ a       accumulate
-        KEY         \ a,c
-        DUP IS-DIGIT?  \ a,c,f
+        TO-DIGIT SWAP 10 * + 
+        KEY DUP IS-DIGIT? 
     0= UNTIL DROP ;
 
-\ read the numbers, print the patterns
 : MAIN
-    GET-NUMBER
-    0 DO
-        GET-NUMBER
-        GET-NUMBER
-        .ROWS
+    GET-NUMBER 0 DO
+        GET-NUMBER GET-NUMBER
+        SWAP .ROWS
     LOOP ;
 
 MAIN BYE

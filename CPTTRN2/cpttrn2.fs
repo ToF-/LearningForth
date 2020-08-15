5ff89cb4c0f4973c687a1a0003dc15c5458d55a7
Christophe Thibaut <cthibaut@octo.com>
2020-08-15 22:14:49 +0200

    refactoring

--- CPTTRN2/cpttrn2.fs
+++ CPTTRN2/cpttrn2.fs
@@ -1,62 +1,46 @@
-CHAR . CONSTANT DOT
-CHAR * CONSTANT STAR
-
-\ true if 0 < i < m-1
-: INSIDE? ( m,i -- f )
+: INSIDE? ( limit,index -- 0|x )
     SWAP 1- OVER - * ;
 
-\ print a star if i is 0 or m-1 else a dot 
-: .DOT-OR-CHAR ( m,i -- c )
-    INSIDE? IF DOT ELSE STAR THEN EMIT ;
-    
-\ print a row of n dots with star at the border
-: .DOTS  ( n -- )
-    0 DO 2R@ .DOT-OR-CHAR LOOP CR ;
+: STARDOT  ( index,limit -- char )
+    INSIDE? IF [CHAR] . ELSE [CHAR] * THEN ;
 
-\ print a row of stars
-: .STARS ( m -- ) 
-    0 DO STAR EMIT LOOP CR ;        
+: .STAR-ROW ( #cols -- )
+    0 DO [CHAR] * EMIT LOOP CR ;
 
-\ print a border row of size m, stars if i = 0 or n-1
-: .ROW ( m,n,i -- )
-    INSIDE? IF .DOTS ELSE .STARS THEN ;
+: .STARDOT-ROW  ( #cols -- )
+    0 DO 2R@ STARDOT EMIT LOOP CR ;
+        
+: .ROW ( #cols,index,limit -- )
+    INSIDE? IF .STARDOT-ROW ELSE .STAR-ROW THEN ;
     
-\ print n rows of dots with a border of stars
-: .ROWS ( n,m -- )
-    SWAP            \ m,n        outer loop max on top 
+: .ROWS ( #cols,#rows -- )
     0 DO DUP 2R@ .ROW LOOP 
     DROP CR ;  
 
-\ true if the given char is between '0' and '9'
-: IS-DIGIT? ( c -- f )
-    DUP  [CHAR] 0 >=
-    SWAP [CHAR] 9 <= AND ;
+: TO-DIGIT ( char -- n )
+    [CHAR] 0 - ;
 
-\ read input until first digit char, return it
-: SKIP-NON-DIGIT ( -- c )
+: IS-DIGIT? ( char -- flag )
+    TO-DIGIT DUP 0 >= SWAP 9 <= AND ;     
+
+: SKIP-NON-DIGIT ( -- char )
     BEGIN
         KEY DUP IS-DIGIT? 0= WHILE
         DROP
     REPEAT ;
 
-\ read input until a number is read, return it
 : GET-NUMBER ( -- n )
-    SKIP-NON-DIGIT  \ c
-    0 SWAP          \ a,c     put the accumulator
+    SKIP-NON-DIGIT  
+    0 SWAP          \ accumulator
     BEGIN
-        [CHAR] 0 -  \ a,d     digit value
-        SWAP 10 * + \ a       accumulate
-        KEY         \ a,c
-        DUP IS-DIGIT?  \ a,c,f
+        TO-DIGIT SWAP 10 * + 
+        KEY DUP IS-DIGIT? 
     0= UNTIL DROP ;
 
-\ read the numbers, print the patterns
 : MAIN
-    GET-NUMBER
-    0 DO
-        GET-NUMBER
-        GET-NUMBER
-        .ROWS
+    GET-NUMBER 0 DO
+        GET-NUMBER GET-NUMBER
+        SWAP .ROWS
     LOOP ;
 
-\ MAIN BYE
+MAIN BYE

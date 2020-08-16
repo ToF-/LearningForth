7d19e69bb6db29007a619d1d58df2253c9f04ba9
Christophe Thibaut <cthibaut@octo.com>
2020-08-16 17:34:39 +0200

    refactoring

--- CPTTRN5/cpttrn5.fs
+++ CPTTRN5/cpttrn5.fs
@@ -7,29 +7,21 @@ VARIABLE SIZE
     SIZE @ SWAP REL-COORD - ;
 
 : DOWN? ( row,col -- flag )
-    SIZE @ 
-    TUCK / 2 MOD
-    -ROT / 2 MOD = ;
+    SIZE @ TUCK / 2 MOD -ROT / 2 MOD = ;
 
 : BORDER? ( row,col -- flag )
-    REL-COORD 0= SWAP 
-    REL-COORD 0= OR ;
+    REL-COORD 0= SWAP REL-COORD 0= OR ;
 
 : DIAGONAL? ( row,col -- char,flag )
-    OVER OVER DOWN? IF
-        REL-COORD SWAP REL-COORD =
-        [CHAR] \
-    ELSE
-        REL-COORD SWAP OPP-COORD =
-        [CHAR] /
-    THEN
-    SWAP ;
+    OVER OVER DOWN? 
+    IF   REL-COORD SWAP REL-COORD = [CHAR] \
+    ELSE REL-COORD SWAP OPP-COORD = [CHAR] / 
+    THEN SWAP ;
     
 : PATTERN ( row,col -- char )
-    OVER OVER BORDER? IF 
-        DROP DROP [CHAR] * 
-    ELSE 
-        DIAGONAL? 0= IF DROP [CHAR] . THEN
+    OVER OVER BORDER? 
+    IF   DROP DROP [CHAR] * 
+    ELSE DIAGONAL? 0= IF DROP [CHAR] . THEN
     THEN ;
 
 : .ROW ( row,limit -- )
@@ -38,8 +30,8 @@ VARIABLE SIZE
 : .ROWS ( #cols,#rows -- )
     0 DO I OVER .ROW CR LOOP DROP ;
 
-: EXPAND ( inner-size+1 -- size )
-    * 1+ ;
+: EXPAND ( inner-size -- size )
+    SIZE @ * 1+ ;
 
 : TO-DIGIT ( char -- n )
     [CHAR] 0 - ;
@@ -48,10 +40,7 @@ VARIABLE SIZE
     TO-DIGIT DUP 0 >= SWAP 9 <= AND ;     
 
 : SKIP-NON-DIGIT ( -- char )
-    BEGIN
-        KEY DUP IS-DIGIT? 0= WHILE
-        DROP
-    REPEAT ;
+    BEGIN KEY DUP IS-DIGIT? 0= WHILE DROP REPEAT ;
 
 : GET-NUMBER ( -- n )
     SKIP-NON-DIGIT  
@@ -63,10 +52,8 @@ VARIABLE SIZE
 
 : MAIN
     GET-NUMBER 0 DO
-        GET-NUMBER GET-NUMBER
-        GET-NUMBER 1+ SIZE !
-        SIZE @ EXPAND SWAP 
-        SIZE @ EXPAND
+        GET-NUMBER GET-NUMBER GET-NUMBER 
+        1+ SIZE ! EXPAND SWAP EXPAND 
         .ROWS CR
     LOOP ;
 

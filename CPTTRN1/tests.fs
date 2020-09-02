2ff50607ccf62ddcde1e3397bf7f95c762cd316b
Christophe Thibaut <cthibaut@octo.com>
2020-09-02 18:14:57 +0200

    start again

--- CPTTRN1/tests.fs
+++ CPTTRN1/tests.fs
@@ -1,50 +1,9 @@
-require ffl/tst.fs
-require cpttrn1.fs
+REQUIRE ffl/tst.fs
+REQUIRE cpttrn1.fs
 
-create output-buffer 10000 allot
-variable output-length
-
-variable input-buffer 
-variable input-length
-variable input-index
-
-\ keep track of each char emitted via output
-: >output-buffer ( char -- )
-    output-buffer output-length @ + c! 
-    1 output-length +! ;
-
-\ make this word the new output behavior
-' >output-buffer is output
-
-\ do nothing is the new NL behavior
-' noop is nl
-
-\ provide the next char received on input
-: >input-buffer ( -- char )
-    input-index @ dup input-length < if 
-        input-buffer @ + c@ 
-        1 input-index +!
-    else
-        drop 4
-    then ;
-
-\ make this word the new input behavior
-' >input-buffer is input
-
-t{ 
-    ." testing the pattern" cr
-    \ save that string as inbut buffer
-    s" 1 4 4" input-length ! input-buffer ! input-index off
-    main
-    \ check the output buffer
-    output-buffer output-length @ 
-    s" *.*..*.**.*..*.*" compare 0 ?s
-}t
-
-t{ 
-    ." getting a number " cr
-    s" 4807" input-length ! input-buffer ! input-index off 
-    get-number
-    4807 ?s
-}t
-bye
+T{  ." STARDOT returns a . or a * depending on arg is odd or even" CR
+    1 STARDOT CHAR . ?S
+    2 STARDOT CHAR * ?S
+    3 STARDOT CHAR . ?S
+}T
+BYE

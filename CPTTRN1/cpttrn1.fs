7f7c755a2fa0d0f1f2b836c59c1983d055e48ea2
Christophe Thibaut <cthibaut@octo.com>
2020-09-02 18:26:50 +0200

    _EMIT emits a char to the output stream unless redirected

--- CPTTRN1/cpttrn1.fs
+++ CPTTRN1/cpttrn1.fs
@@ -1,2 +1,4 @@
 : STARDOT ( n -- char )
     1 AND IF [CHAR] . ELSE [CHAR] * THEN ;
+
+DEFER _EMIT  ' EMIT IS _EMIT
--- CPTTRN1/tests.fs
+++ CPTTRN1/tests.fs
@@ -6,4 +6,19 @@ T{  ." STARDOT returns a . or a * depending on arg is odd or even" CR
     2 STARDOT CHAR * ?S
     3 STARDOT CHAR . ?S
 }T
+
+T{ ." _EMIT emits a char to the output stream unless redirected " CR 
+    CREATE TEMP 10000 ALLOT
+    VARIABLE TEMP-LENGTH
+
+    : >TEMP ( char )
+        TEMP TEMP-LENGTH @ + C! 
+        1 TEMP-LENGTH +! ;
+
+    ' >TEMP IS _EMIT
+    TEMP-LENGTH OFF
+    CHAR ! _EMIT CHAR $ _EMIT CHAR # _EMIT
+    S" !$#" TEMP TEMP-LENGTH @ COMPARE 0 ?S
+}T
+
 BYE

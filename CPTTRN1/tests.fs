9410b8f407e1c4cf3d6a4275aa6595dc42de023d
Christophe Thibaut <cthibaut@octo.com>
2020-09-02 20:06:57 +0200

    refactoring to allow for distinct length of input and output during tests

--- CPTTRN1/tests.fs
+++ CPTTRN1/tests.fs
@@ -9,23 +9,23 @@ T{  ." STARDOT returns a . or a * depending on arg is odd or even" CR
 
 T{ ." _EMIT emits a char to the output stream unless redirected " CR 
     CREATE TEMP 10000 ALLOT
-    VARIABLE TEMP-LENGTH
+    VARIABLE OUTPUT-LENGTH
 
     : >TEMP ( char )
-        TEMP TEMP-LENGTH @ + C! 
-        1 TEMP-LENGTH +! ;
+        TEMP OUTPUT-LENGTH @ + C! 
+        1 OUTPUT-LENGTH +! ;
 
     ' >TEMP IS _EMIT
-    TEMP-LENGTH OFF
+    OUTPUT-LENGTH OFF
     CHAR ! _EMIT CHAR $ _EMIT CHAR # _EMIT
-    S" !$#" TEMP TEMP-LENGTH @ COMPARE 0 ?S
+    S" !$#" TEMP OUTPUT-LENGTH @ COMPARE 0 ?S
 }T
 
 T{ ." .ROW displays a row of alternate dots and stars " CR
     ' >TEMP IS _EMIT
-    TEMP-LENGTH OFF
+    OUTPUT-LENGTH OFF
     0 4 .ROW 
-    S" *.*." TEMP TEMP-LENGTH @ COMPARE 0 ?S
+    S" *.*." TEMP OUTPUT-LENGTH @ COMPARE 0 ?S
  }T
 
 T{ ." _CR emits a new line to the output stream unless redirected " CR
@@ -33,24 +33,25 @@ T{ ." _CR emits a new line to the output stream unless redirected " CR
     
     ' TEMP-CR IS _CR
     0 TEMP C!
-    TEMP-LENGTH OFF
+    OUTPUT-LENGTH OFF
     _CR TEMP C@ 13 ?S
 }T
 
 T{ ." .ROWS emits several rows of alternate dots and stars " CR
     TEMP 10000 ERASE
-    TEMP-LENGTH OFF
+    OUTPUT-LENGTH OFF
     ' >TEMP IS _EMIT
     ' NOOP IS _CR
     4 4 .ROWS
-    S" *.*..*.**.*..*.*" TEMP TEMP-LENGTH @ COMPARE 0 ?S
+    S" *.*..*.**.*..*.*" TEMP OUTPUT-LENGTH @ COMPARE 0 ?S
 }T
 
 T{ ." _KEY reads a char from the input stream unless redirected " CR
     VARIABLE TEMP-INPUT
+    VARIABLE INPUT-LENGTH
     VARIABLE TEMP-INDEX
     : <TEMP ( -- char )
-        TEMP-INDEX @ TEMP-LENGTH @ < IF
+        TEMP-INDEX @ INPUT-LENGTH @ < IF
             TEMP-INPUT @ TEMP-INDEX @ + C@ 
             1 TEMP-INDEX +! 
         ELSE
@@ -58,18 +59,19 @@ T{ ." _KEY reads a char from the input stream unless redirected " CR
         THEN ;
 
     ' <TEMP IS _KEY
-    S" 42" TEMP-LENGTH ! TEMP-INPUT ! TEMP-INDEX OFF
+    S" 42" INPUT-LENGTH ! TEMP-INPUT ! TEMP-INDEX OFF
     _KEY CHAR 4 ?S _KEY CHAR 2 ?S
 }T
 
 T{ ." SKIP-NON-DIGITS reads chars from the input stream until a digit it read " CR
 
-    S"    42" TEMP-LENGTH ! TEMP-INPUT ! TEMP-INDEX OFF
+    S"    42" INPUT-LENGTH ! TEMP-INPUT ! TEMP-INDEX OFF
     SKIP-NON-DIGITS CHAR 4 ?S
 }T
 
 T{ ." GET-NUMBER reads a number from the input stream " CR 
-    S" 4807" TEMP-LENGTH ! TEMP-INPUT ! TEMP-INDEX OFF
+    S" 4807" INPUT-LENGTH ! TEMP-INPUT ! TEMP-INDEX OFF
     GET-NUMBER 4807 ?S
 }T
+
 BYE
